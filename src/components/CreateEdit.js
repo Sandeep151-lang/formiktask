d4d849b0d558335bdcc8d4c10dab28a9d377a277
Sandeep <sandeepnandanwar92@gmail.com>
2021-10-04 12:12:15 +0530

    first commit

--- src/components/CreateEdit.js
+++ src/components/CreateEdit.js
@@ -5,34 +5,10 @@ import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
 
-
-const validationSchema = yup.object({
-    fname: yup.string().required("Required"),
-    lname: yup.string().required("Required"),
-    email: yup
-        .string()
-        .email("Enter a valid email")
-        .required("Email is required"),
-});
 
 // Add User details component 
 const CreateEdit = () => {
-
-    const formik = useFormik({
-        initialValues: {
-            fname: '',
-            lname: '',
-            email: 'foobar@gmail.com'
-        },
-        validationSchema: validationSchema,
-        onSubmit: (values) => {
-            values.preventDefault();
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
 
 
     const url = `https://61485ca2035b3600175b9dc6.mockapi.io/api/v1/users`;
@@ -79,35 +55,18 @@ const CreateEdit = () => {
     return <>
         <div className="container text-center"><h1 className="font-weight-bold text-dark">Add User</h1></div>
         <Container className="themed-container" style={{ "width": "70%" }}>
-            <Form method="POST" onSubmit={(e) => {
-                formik.handleSubmit(e)
-                submit(e)
-
-            }}>
+            <Form method="POST">
                 <FormGroup row className="py-3">
-                    <TextField color="warning" id="fname" label="First name" type="text" variant="filled" name="fname" value={formik.values.fname} onChange={(e) => {
-                        formik.handleChange(e)
-                        handlevent(e)
-                    }} autoComplete="off" required error={formik.touched.fname && Boolean(formik.errors.fname)}
-                        helperText={formik.touched.fname && formik.errors.fname} />
+                    <TextField color="warning" id="fname" label="First name" type="text" variant="filled" name="fname" value={form_data.fname} onChange={ handlevent } autoComplete="off" required  />
                 </FormGroup>
                 <FormGroup row className="py-3">
-                    <TextField required color="warning" id="lname" label="Last name" type="text" variant="filled" name="lname" value={form_data.lname} onChange={(e) => {
-                        formik.handleChange(e)
-                        handlevent(e)
-                    }} error={formik.touched.lname && Boolean(formik.errors.lname)}
-                        helperText={formik.touched.lname && formik.errors.lname} />
+                    <TextField required color="warning" id="lname" label="Last name" type="text" variant="filled" name="lname" value={form_data.lname} onChange={handlevent} />
                 </FormGroup>
                 <FormGroup row className="py-3">
-                    <TextField required color="warning" id="email_id" label="Email_id" type="email" variant="filled" name="email" value={formik.values.email_id}
-                        onChange={(e) => {
-                            formik.handleChange(e)
-                            handlevent(e)
-                        }} error={formik.touched.email && Boolean(formik.errors.email)}
-                        helperText={formik.touched.email && formik.errors.email} />
+                    <TextField required color="warning" id="email_id" label="Email_id" type="email" variant="filled" name="email" value={form_data.email_id} onChange={handlevent} />
                 </FormGroup>
                 <FormGroup row>
-                    <Button className="mt-3 py-1" style={{ "width": "100%" }} variant="contained" type="submit">Submit</Button>
+                    <Button className="mt-3 py-1" style={{ "width": "100%" }} variant="contained" onClick={(e)=>submit(e)}>Submit</Button>
                 </FormGroup>
                 <ToastContainer />
             </Form>
